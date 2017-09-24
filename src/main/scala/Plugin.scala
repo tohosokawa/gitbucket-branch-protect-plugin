2fb18e9b95869907e5b50fde5d758bcd197f9b68
tohosokawa <tohosokawa1979@gmail.com>
2017-09-24 18:38:10 +0900

    Remove HelloWorld

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -1,4 +1,3 @@
-import com.github.tohosokawa.HelloWorldController
 import com.github.tohosokawa.ProtectedBranchService.ProtectedBranchReceiveHook
 import io.github.gitbucket.solidbase.model.Version
 
@@ -7,10 +6,6 @@ class Plugin extends gitbucket.core.plugin.Plugin {
   override val pluginName: String = "Branch Protect Plugin"
   override val description: String = "Disables force and non force-pushes to this branch and prevents it from being deleted."
   override val versions: List[Version] = List(new Version("1.0.0"))
-
-  override val controllers = Seq(
-    "/*" -> new HelloWorldController()
-  )
 
   override val receiveHooks = Seq(
     new ProtectedBranchReceiveHook()
