61486402bbb1a368778f1ae3c1e4b898b99abff3
tohosokawa <tomotake.hosokawa@gmail.com>
2020-09-22 05:33:18 +0000

    Update scala version

--- build.sbt
+++ build.sbt
@@ -2,12 +2,12 @@ name := "gitbucket-branch-protect-plugin"
 
 organization := "com.github.tohosokawa"
 
-version := "1.0.1"
+version := "1.1.0"
 
-scalaVersion := "2.12.2"
+scalaVersion := "2.13.3"
 
 libraryDependencies ++= Seq(
-  "io.github.gitbucket" %% "gitbucket"         % "4.16.0" % "provided",
+  "io.github.gitbucket" %% "gitbucket"         % "4.32.0" % "provided",
   "javax.servlet"        % "javax.servlet-api" % "3.1.0"  % "provided"
 )
 
--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -5,7 +5,12 @@ class Plugin extends gitbucket.core.plugin.Plugin {
   override val pluginId: String = "branch-protect"
   override val pluginName: String = "Branch Protect Plugin"
   override val description: String = "Disables force and non force-pushes to this branch and prevents it from being deleted."
-  override val versions: List[Version] = List(new Version("1.0.0"),new Version("1.0.1"))
+  override val versions: List[Version] = List(
+    // 4.15.0 - 4.31.2
+    new Version("1.0.0"),new Version("1.0.1"),
+    // 4.32.0 -
+    new Version("1.1.0")
+  )
 
   override val receiveHooks = Seq(
     new ProtectedBranchReceiveHook()
